0cae7c7641b9691ca602cf0f9dd8a798fbefc534
RC7502 <dev@bitsmack.com>
2014-01-26 01:22:43 -0500

    filled in summary info

--- BitsmackGTWeb/Scripts/Pedometer.js
+++ BitsmackGTWeb/Scripts/Pedometer.js
@@ -9,7 +9,18 @@ function getPedometerSummary() {
         { 
             url: "http://bitsmackgtapi.apphb.com/pedometer/summary",
             success:    function (returndata) {
-                $("#pedometersummary").html(returndata.NewStepGoal);
+                $("#numofdays").html(returndata.NumOfDays);
+                $("#avgsteps").html(returndata.AverageSteps);
+                $("#trendsteps").html(returndata.TrendSteps);
+                var direction;
+                if (returndata.AverageSteps > returndata.TrendSteps) {
+                    direction = "Negative :-(";
+                }
+                else {
+                    direction = "Positive!";
+                }
+                $("#directionsteps").html(direction);
+                $("#newstepgoal").html(returndata.NewStepGoal);
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 $("#pedometersummary").html(textStatus + "" + errorThrown);
