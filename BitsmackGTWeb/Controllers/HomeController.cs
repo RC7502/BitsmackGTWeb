8e5c83947f98fbfb5b00569af4ce9b8a83d9dee1
RC7502 <dev@bitsmack.com>
2014-01-26 00:35:50 -0500

    started Pedometer view

--- BitsmackGTWeb/Controllers/HomeController.cs
+++ BitsmackGTWeb/Controllers/HomeController.cs
@@ -19,5 +19,10 @@ namespace BitsmackGTWeb.Controllers
         {
             return View();
         }
+
+        public ActionResult Pedometer()
+        {
+            return View();
+        }
     }
 }
