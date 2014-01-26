46a45a6ae7f170d87bba68a5283422037cdceb91
RC7502 <dev@bitsmack.com>
2014-01-26 01:04:57 -0500

    connect to API for pedometer summary

--- BitsmackGTWeb/Scripts/Pedometer.js
+++ BitsmackGTWeb/Scripts/Pedometer.js
@@ -1,4 +1,18 @@
 ﻿$(function () {
-
+    getPedometerSummary();
 
 });
+
+function getPedometerSummary() {
+
+    $.ajax(
+        { 
+            url: "http://bitsmackgtapi.apphb.com/pedometer/summary",
+            success:    function (returndata) {
+                $("#pedometersummary").html(returndata.NewStepGoal);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#pedometersummary").html(textStatus + "" + errorThrown);
+            }
+        });
+}
